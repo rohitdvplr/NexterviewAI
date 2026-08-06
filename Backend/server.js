aa42df0b742e1d86711c5851699046b70eef294f
Rohit Gupta <rohitgupta00699@gmail.com.com>
2026-08-06 15:43:38 +0530

    Production backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,15 @@
-import dotenv from "dotenv";
-dotenv.config();
-import app from "./src/app.js";
-import connectDb from "./src/config/database.js";
+  import dotenv from "dotenv";
+  dotenv.config();
+  import app from "./src/app.js";
+  import connectDb from "./src/config/database.js";
 
 
 
-connectDb();
+  connectDb();
 
 
+  const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
